432d18ae7dfc5e1cc02c790b19533717d87401d1
balhan84 <balamutenkoanna@gmail.com>
2024-05-11 15:47:23 +0300

    Add templateLiteral

---
+++ assets/js/index.js
@@ -0,0 +1,65 @@
+// alert("Hello world 2");
+
+// let, const, var
+
+// let age = 18;
+// alert(age);
+// age = 19;
+// alert(age);
+
+// const count = 10;
+// count = 11;
+// alert(count);
+
+// let bigNumber = 99999999999999999999999n;
+// alert(bigNumber);
+
+// let userName = "Ivo";
+// alert(userName);
+
+// let phrase = "His name is 'Ivo'";
+// alert(phrase);
+// let someHtml = `<h1>Head</h1>
+// <p>Pharagraph</p>
+// `;
+
+// document.write(someHtml);
+
+// boolean - true, false
+// let isEmail = true;
+// let gender = "female";
+// let isMale = true;
+
+// let myName = "Anna";
+// let myEmail = "anna@gmail.com";
+// let myAge = 39;
+// // let gender = true;
+
+// alert("myEmail:" + myEmail);
+// console.log("myName:", myName, "myEmail:", myEmail);
+
+// let totalPrice = 2 * 500 + 3 * 200;
+// console.log("Total:", totalPrice, "hrn");
+
+// let amount1 = 2;
+// let price1 = 500;
+// let amount2 = 3;
+// let price2 = 200;
+
+// let totalPrice = amount1 * price1 + amount2 * price2;
+// console.log("Total:", totalPrice, "hrn");
+
+// const firstName = prompt("Input name:", "anonim");
+// const lastName = prompt("Input surname:", "noname");
+// alert(firstName + " " + lastName);
+
+// document.write("<h2>" + firstName + " " + lastName + "</h2>");
+
+// const firstNumber = Number(prompt("1 number:", "0"));
+// const secondNumber = +prompt("2 number:", "0");
+// const sum = firstNumber + secondNumber;
+// console.log("sum:>>", sum);
+
+const amountItems = Number(prompt("amountItems:", "0"));
+const price = 200;
+console.log("totalPrice:", amountItems * price);
---
+++ assets/js/templateLiteral.js
@@ -0,0 +1,15 @@
+// alert("Literal");
+
+const greeting = "Hello";
+const name = "Test";
+
+const resultString = `${greeting} ${name}!`;
+console.log(resultString);
+
+const a = 10;
+const b = 5;
+// const sum = a + b;
+// console.log(a + b);
+
+const resultSum = `${a} + ${b} = ${a + b}`;
+console.log(resultSum);
