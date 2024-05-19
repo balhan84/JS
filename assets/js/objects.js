0c83618574bb028bc080f95c02b161b7c7a18157
balhan84 <balamutenkoanna@gmail.com>
2024-05-19 15:10:25 +0300

    Add objects.js

---
+++ assets/js/objects.js
@@ -0,0 +1,4 @@
+const newsTitle1 = "Lorem ipsum dolor sit amet";
+const newsBody1 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+const newsDate1 = new Date("2024-05-18");
