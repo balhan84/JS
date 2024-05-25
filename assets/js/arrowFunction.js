98c02fbceb12f379db2d7bdba918733052924c2c
balhan84 <balamutenkoanna@gmail.com>
2024-05-25 19:34:29 +0300

    Add arrow

---
+++ assets/js/arrowFunction.js
@@ -0,0 +1,18 @@
+// function sum (a,b) {
+//     return a+b;
+// }
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// const isAdult = (age) => {
+//   return age >= 18;
+// };
+
+// const sum = (a, b) => a + b;
+
+// const packObject = (firstName, lastName) => ({
+//   firstName: firstName,
+//   lastName: lastName,
+// });
